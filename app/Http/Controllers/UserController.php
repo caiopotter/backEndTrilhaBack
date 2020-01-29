dea1bb75802c6dc6610f6e4b8779eff4cfba740d
caiopotter <limapotter@gmail.com>
2020-01-29 19:17:41 -0300

    Post User

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    public function searchByEmail(Request $request)
+    {
+        return User::whereLike('email', $request->param)->get();
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::insert($request->all());
+
+        return response()->json($user, 201);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,58 +2,7 @@
 
 use Illuminate\Http\Request;
 Use App\Movie;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-/*
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('movies', function() {
-    // If the Content-Type and Accept headers are set to 'application/json', 
-    // this will return a JSON structure. This will be cleaned up later.
-    return Movie::all();
-});
- 
-Route::get('movies/{id}', function($id) {
-    return Movie::find($id);
-});
-
-Route::get('movies/{id}', function($id) {
-    return Movie::find($id);
-});
-
-Route::post('movies', function(Request $request) {
-    return Movie::create($request->all);
-});
-
-Route::put('movies/{id}', function(Request $request, $id) {
-    $movie = Movie::findOrFail($id);
-    $movie->update($request->all());
-
-    return $movie;
-});
-
-Route::delete('movies/{id}', function($id) {
-    Movie::find($id)->delete();
-
-    return 204;
-});*/
-
-/*Route::get('movies', 'MovieController@index');
-Route::get('movies/{id}', 'MovieController@show');
-Route::post('movies', 'MovieController@store');
-Route::put('movies/{id}', 'MovieController@update');
-Route::delete('movies/{id}', 'MovieController@delete');*/
+Use App\User;
 
 Route::get('movies', 'MovieController@index');
 Route::get('movies/{movie}', 'MovieController@show');
@@ -62,3 +11,8 @@ Route::get('moviesSearchTitle', 'MovieController@searchByTitle');
 Route::post('movies', 'MovieController@store');
 Route::put('movies/{movie}', 'MovieController@update');
 Route::delete('movies/{movie}', 'MovieController@delete');
+
+Route::get('users', 'UserController@index');
+Route::get('users/{user}', 'UserController@show');
+Route::get('usersSearchByEmail', 'UserController@searchByEmail');
+Route::post('users', 'UserController@store');
